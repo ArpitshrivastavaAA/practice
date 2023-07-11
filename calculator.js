59d5d7895950e591eb66a52722675824a715e9e2
@ArpitshrivastavaAA <arpit2137@gmail.com>
2023-07-11 11:10:55 +0530

    hello calculator

---
+++ calculator.js
@@ -0,0 +1,39 @@
+// let num1,num2,operator,result;
+// num1=parseFloat(prompt("enter the first number:"));
+// num2=parseFloat(prompt("enter the second number:"));
+// operator=prompt("enter the operator(+,-,*,/):");
+
+// switch (operator) {
+//     case "+":
+//         result=num1+num2;
+//         break;
+//         case "-":
+//             result=num1-num2;
+//             break;
+//             case "*":
+//                 result=num1*num2;
+//                 break;
+//                 case "/":
+//                     result=num1/num2;
+//                     break;
+
+
+//     default:
+//         alert("invalid operator")
+//         break;
+// }
+// alert("Result:" +result)
+
+// let text=document.getElementById("text")
+// let  step=document.getElementById("step")
+// let play=document.getElementById("play")
+
+// play.addEventListener('click',()=>{
+//     talk(text.value)
+// })
+
+// function talk(pow){
+//     let rex=new SpeechSynthesisUtterance(pow)
+//     rex.rate=step.value
+//     speechSynthesis.speak(rex)
+// }
